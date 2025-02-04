599fe4f7ea63d5ee672d5ecf4c78974618d6d746
Jose Victor <ragjvictor@gmail.com>
2025-02-04 02:55:44 -0400

    Ajuste no spawn dos inimigos em terreno válido

--- scripts/manager/EnemyManager.cs
+++ scripts/manager/EnemyManager.cs
@@ -1,9 +1,12 @@
 using Godot;
+using Godot.Collections;
 using System;
+using System.Linq;
 
 // Gerenciador de inimigos do jogo
-public partial class EnemyManager : Node
+public partial class EnemyManager : Node 
 {
+
 	[Export]
 	public PackedScene basicEnemyScene; // Cena do inimigo básico a ser instanciada
 
@@ -16,7 +19,7 @@ public partial class EnemyManager : Node
 
 	const int SpawnRadius = 330; // Raio de spawn dos inimigos
 
-	public override void _Ready()
+	public override void _Ready() 
 	{
 		timer = GetNode<Timer>("Timer");
 		baseSpawnTime = timer.WaitTime;
@@ -24,29 +27,62 @@ public partial class EnemyManager : Node
 		timer.Timeout += OnTimerTimeout; // Associa o evento de timeout ao método OnTimerTimeout
 	}
 
+	private Vector2 GetSpawnPosition() 
+	{
+		Node2D player = (Node2D)GetTree().GetFirstNodeInGroup("player"); // Obtém o jogador
+		if (player == null) 
+		{
+			return Vector2.Zero;
+		}
+
+		Vector2 spawnPosition = Vector2.Zero;
+		Vector2 randomDirection = Vector2.Right.Rotated((float)GD.RandRange(0, Math.Tau));
+
+
+		// Loop para buscar uma área de spawn válida para os inimigos
+		foreach (int i in Enumerable.Range(0,4))
+		{
+			// Gera uma direção aleatória para o spawn
+			spawnPosition = player.GlobalPosition + (randomDirection * SpawnRadius); // Calcula a posição de spawn
+
+			// Verifica se a posição do spawn não está em conflito com as areas de colisão do layer do Terreno
+			var queryParameters = PhysicsRayQueryParameters2D.Create(player.GlobalPosition, spawnPosition, 1);
+			Dictionary result = GetTree().Root.World2D.DirectSpaceState.IntersectRay(queryParameters);
+
+			if (result.Count == 0)
+			{
+				break;
+			}
+			else
+			{
+				randomDirection = randomDirection.Rotated(Mathf.DegToRad(90));
+			}
+		}
+
+		return spawnPosition;
+		
+	}
+
 	// Método chamado quando o timer atinge o tempo limite
-	private void OnTimerTimeout()
+	private void OnTimerTimeout() 
 	{
 		timer.Start();
 
 		Node2D player = (Node2D)GetTree().GetFirstNodeInGroup("player"); // Obtém o jogador
-		if (player == null)
+		if (player == null) 
 		{
 			return;
 		}
-
-		// Gera uma direção aleatória para o spawn
-		Vector2 randomDirection = Vector2.Right.Rotated((float)GD.RandRange(0, Math.Tau));
-		Vector2 spawnPosition = player.GlobalPosition + (randomDirection * SpawnRadius); // Calcula a posição de spawn
+		
 
 		Node2D enemy = (Node2D)basicEnemyScene.Instantiate(); // Instancia o inimigo
 
 		Node2D entitiesLayer = (Node2D)GetTree().GetFirstNodeInGroup("entities_layer"); // Obtém o grupo de entidades
 		entitiesLayer.AddChild(enemy); // Adiciona o inimigo ao grupo de entidades
-		enemy.GlobalPosition = spawnPosition; // Define a posição do inimigo
+		enemy.GlobalPosition = GetSpawnPosition(); // Define a posição do inimigo
 	}
 
-	private void OnArenaDificultyIncreased(int arenaDificulty)
+	private void OnArenaDificultyIncreased(int arenaDificulty) 
 	{
 		float timeOff = (0.1f/12) * arenaDificulty;
 		timeOff = Math.Min(timeOff, 0.7f);
